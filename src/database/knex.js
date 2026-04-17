715f1942734c367a57bfae91fb9bac10c3e7b58e
NATHAN DA SILVA COSTA <25132656@dsn127m1246732.cfp127aluno.local>
2026-04-17 08:46:55 -0300

    chore: configura inicialmente o express, cors, knex, e rota para consumo do arquivo do swagger

---
+++ src/database/knex.js
@@ -0,0 +1,12 @@
+const knex = require('knex')({
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: Number(process.env.DB_PORT) || 3306,
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_NAME || 'db_escopo',
+  },
+})
+
+module.exports = knex
